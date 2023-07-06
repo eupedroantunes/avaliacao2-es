ad77acbb4592f4902f2adee03560d3ab0e8cdd6a
eupedroantunes <phnantunes@gmail.com>
2023-07-05 22:10:18 -0300

    Add typeorm imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,7 +11,16 @@ import { ContentsModule } from './modules/contents/contents.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT || 5432,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DB,
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      // logging: true,
+    }),
     GraphQLModule.forRoot({
       debug: false,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
